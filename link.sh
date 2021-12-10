b3cc4a9cdd0ed8bb3da3f36de7cecf65053f4eed
Andrew Yoon <andrewy563@gmail.com>
2021-12-10 15:11:02 -0800

    add command to initiate submodules

--- link.sh
+++ link.sh
@@ -1,3 +1,5 @@
 # script to create sym links between configs contained inside repository to the home directory
 ln -s ~/vimsettings/.vimrc ~/.vimrc
 ln -s ~/vimsettings/.vim ~/.vim
+
+git submodule update --init
